a50808e14811cf23c74b9963b66d47a2dcaf9d12
varunkumarnr <varunnrk@gmail.com>
2020-08-08 16:52:04 +0530

    video call working

--- public/script.js
+++ public/script.js
@@ -1,28 +1,49 @@
-const videoGrid = document.getElementById('video-grid');
-var socket = io();
-const myVideo = document.createElement('video');
-myVideo.muted = true;
-
-
-let myVideoStream ;
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer(undefined, 
+    {host:'peerjs-server.herokuapp.com', secure:true, port:443}
+  )
+const myVideo = document.createElement('video')
+myVideo.muted = true
+const peers = {}
 navigator.mediaDevices.getUserMedia({
-    video:true,
-    audio:true
-}).then(stream =>{
-    myVideoStream =  stream;
-    addVideoSteam(myVideo,stream)
-})
-socket.emit('join-room',ROOM_ID);
-socket.on('user-connected',()=>{
-    connectNewUser();
-})
-const connectNewUser = () =>{
-    console.log('new user');
-}
-const addVideoSteam = (video,stream) =>{
-    video.srcObject = stream;
-    video.addEventListener('loadedmetadata',()=>{
-        video.play();
+  video: true,
+  audio: false
+}).then(stream => {
+  addVideoStream(myVideo, stream)
+
+  myPeer.on('call', call => {
+    call.answer(stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
     })
-    videoGrid.append(video);
+  })
+
+  socket.on('user-connected', userId => {
+    connectToNewUser(userId, stream)
+  })
+})
+
+
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
+ 
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video)
 }
--- server.js
+++ server.js
@@ -1,30 +1,27 @@
-const express =  require('express');
-const app = express();
-const server =  require('http').Server(app);
-const {v4: uuidv4} = require('uuid') ;
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { ExpressPeerServer } = require('peer');
+const { v4: uuidV4 } = require('uuid')
 
-const peerServer = ExpressPeerServer(server,{
-    debug: true
-});
-app.set('view engine','ejs');
-app.use('/peerjs',peerServer);
-app.use(express.static('public'));
-app.use(express.static('public/style.css'));
-app.get('/',(req,res)=>{
-    res.redirect(`/${uuidv4()}`);
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
 })
-app.get('/:room',(req,res)=>{
-    res.render('room',{roomId: req.params.room})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
 })
+
 io.on('connection', socket => {
-    socket.on('join-room',(roomId)=>{
-    socket.join(roomId);
-    socket.to(roomId).broadcast.emit('user-connected')
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+
+ 
+  })
 })
-  });
 
-
-
-server.listen(3030);
+server.listen(3030)
