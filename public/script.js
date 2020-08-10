49c15b858409966d74405adf26f339633695173a
varunkumarnr <varunnrk@gmail.com>
2020-08-10 18:09:27 +0530

    final

--- public/script.js
+++ public/script.js
@@ -1,15 +1,19 @@
+
+
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, 
     {host:'peerjs-server.herokuapp.com', secure:true, port:443}
   )
+  let myVideoStream;
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
 navigator.mediaDevices.getUserMedia({
   video: true,
-  audio: false
+  audio: true
 }).then(stream => {
+  myVideoStream = stream;
   addVideoStream(myVideo, stream)
 
   myPeer.on('call', call => {
@@ -23,6 +27,10 @@ navigator.mediaDevices.getUserMedia({
   socket.on('user-connected', userId => {
     connectToNewUser(userId, stream)
   })
+  socket.on('createMessage',message=>{
+    $('.messages').append(`<li class="message"><b>user</b><br/>${message}</li>`)
+    scrollToBottom();
+ })
 })
 
 
@@ -37,6 +45,7 @@ function connectToNewUser(userId, stream) {
   call.on('stream', userVideoStream => {
     addVideoStream(video, userVideoStream)
   })
+
  
 }
 
@@ -46,4 +55,71 @@ function addVideoStream(video, stream) {
     video.play()
   })
   videoGrid.append(video)
+}
+let text = $("input");
+
+
+
+$('html').keydown((e)=>{
+  if(e.which === 13 && text.val().length!== 0){
+    console.log(text.val());
+    socket.emit('message',text.val());
+    text.val('');
+  }
+})
+const scrollToBottom = () => {
+  var d = $('.chat-screen');
+  d.scrollTop(d.prop("scrollHeight"));
+}
+const muteandmute = () => {
+  console.log(myVideoStream);
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+}
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `
+  document.querySelector('.main_mute').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `
+  document.querySelector('.main_mute').innerHTML = html;
+}
+
+const PlayandStopVideo = () => {
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if(enabled){
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo()
+  }else{
+    myVideoStream.getVideoTracks()[0].enabled = true ;
+    setStopVideo();
+  }
+}
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `
+  document.querySelector('.main_video').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `
+  document.querySelector('.main_video').innerHTML = html;
 }
--- server.js
+++ server.js
@@ -19,9 +19,11 @@ io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
     socket.to(roomId).broadcast.emit('user-connected', userId)
-
+    socket.on('message',message=>{
+        io.to(roomId).emit('createMessage',message)
+     })
  
   })
 })
 
-server.listen(3030)
+server.listen(process.env.PORT||3030)
